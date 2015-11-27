28c909a0bfab842ea6129702665f5ab0795bed8e
Tellicious <Tellicious@users.noreply.github.com>
2015-11-27 13:39:10 +0100

    Changed readMultipleRegisters method
    Changed readMultipleRegisters method to use a while loop instead of a for one

--- ADXL345.cpp
+++ ADXL345.cpp
@@ -62,8 +62,8 @@ void ADXL345::readMultipleRegisters(uint8_t chipSelectPin, uint8_t * buffer, uin
 	startRegister |= (ADXL345_READ | ADXL345_MULT);// register in multiple read mode
 	digitalWrite(chipSelectPin, LOW);	// ChipSelect low to select the chip
 	SPI.transfer(startRegister);		// send the command to read thisRegister
-	for (uint8_t ii = 0; ii < number_of_registers; ii++){
-		buffer[ii] = SPI.transfer(0x00);
+	while(number_of_registers--){
+		*buffer++ = SPI.transfer(0x00);
 	}
 	digitalWrite(chipSelectPin, HIGH);	// ChipSelect high to deselect the chip
 	return;
